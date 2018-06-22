a5736b8aa87ca8a458962bd9e7d865d934743ac0
pat-s <patrick.schratz@gmail.com>
2018-06-22 13:14:36 +0200

    force namespace from mlr for propVectorToMatrix

--- R/RLearner_classif_gam.R
+++ R/RLearner_classif_gam.R
@@ -83,7 +83,7 @@ predictLearner.classif.gam = function(.learner, .model, .newdata, ...) {
   x = predict(.model$learner.model, newdata = .newdata, type = "response", ...)
   levs = .model$task.desc$class.levels
   if (.learner$predict.type == "prob") {
-    propVectorToMatrix(x, levs)
+    mlr:::propVectorToMatrix(x, levs)
   } else {
     levs = .model$task.desc$class.levels
     p = as.factor(ifelse(x > 0.5, levs[2L], levs[1L]))
