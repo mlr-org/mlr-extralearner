22d07a36b429b4fd4864504e95f96a3201203347
pfistfl <pfistererf@googlemail.com>
2019-01-22 13:05:05 +0100

    Add params

--- R/RLearner_classif_autosklearn.R
+++ R/RLearner_classif_autosklearn.R
@@ -1,17 +1,31 @@
+autosklearn_estimators = c("adaboost", "bernoulli", "random_forest", "decision_tree", "lda",
+  "extra_trees", "gaussian_nb", "gradient_boosting", "k_nearest_neighbours", "liblinear_svc", "libsvm_svc",
+  "multinomlial_nb", "passive_aggressive", "qda", "sgd", "xgradient_boosting")
+# Attention: Some preprocessors are missing
+autosklearn_preprocessors = c("no_preprocessing", "densifier", "fast_ica")
+
 #' @export
 makeRLearner.classif.autosklearn = function() {
   makeRLearnerClassif(
     cl = "classif.autosklearn",
     package = "reticulate",
+    # For full paramset see https://automl.github.io/auto-sklearn/master/api.html
+    # Attention: Defaults are not exactly as in  autosklearn
     par.set = makeParamSet(
-      makeIntegerLearnerParam("time_left_for_this_task", lower = 1L, upper = Inf, default = 120L),
-      makeIntegerLearnerParam("ensemble_size", lower = 0L, upper = Inf, default = 1L),
-      makeIntegerLearnerParam("initial_configurations_via_metalearning", lower = 0L, upper = Inf, default = 1L)
+      makeIntegerLearnerParam("time_left_for_this_task", lower = 1L, upper = Inf, default = 3600L),
+      makeIntegerLearnerParam("per_run_time_limit", lower = 1L, upper = Inf, default = 360L),
+      makeIntegerLearnerParam("initial_configurations_via_metalearning", lower = 0L, upper = Inf, default = 25L),
+      makeUntypedLearnerParam("include_estimators", default = list("random_forest"), special.vals = list(NULL)),
+      makeUntypedLearnerParam("include_preprocessors", default = list("no_preprocessing"), special.vals = list(NULL)),
+      makeUntypedLearnerParam("exclude_estimators", default = NULL, special.vals = list(NULL)),
+      makeUntypedLearnerParam("exclude_preprocessors", default = NULL, special.vals = list(NULL)),
+      makeIntegerLearnerParam("ensemble_size", lower = 0L, upper = Inf, default = 50L),
+      makeIntegerLearnerParam("ensemble_nbest", lower = 0L, upper = Inf, default = 50L)
     ),
     properties = c("twoclass", "multiclass", "numerics", "prob", "missings", "factors"),
     name = "Autosklearn",
     short.name = "autosklearn",
-    note = ""
+    note = "Defaults deviate from autosklearn defaults"
     )
 }
 
@@ -20,10 +34,7 @@ trainLearner.classif.autosklearn = function(.learner, .task, .subset, .weights =
 
   autosklearn = import("autosklearn")
 
-  classifier = autosklearn$classification$AutoSklearnClassifier(
-     include_estimators=list("random_forest"), exclude_estimators=NULL,
-     include_preprocessors = list("no_preprocessing"), exclude_preprocessors=NULL,
-     ...)
+  classifier = autosklearn$classification$AutoSklearnClassifier(...)
 
   train = getTaskData(.task, .subset, target.extra = TRUE)
   feat.type = ifelse(vlapply(train$data, is.factor), "Categorical", "Numerical")
